608df80d298d86afe3313357da5b84c208b37879
Jason Cartwright <jason.cartwright44@gmail.com>
2019-04-27 22:46:02 -0700

    fix for parse

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articlesScraper";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
