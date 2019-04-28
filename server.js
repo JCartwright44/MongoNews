4337fd929e05546ffbf1da890e708b6bd4faa552
Jason Cartwright <jason.cartwright44@gmail.com>
2019-04-27 22:39:39 -0700

    mongo changes

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articlesScraper", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articlesScraper";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
